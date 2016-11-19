1380cb867c5487c82c3521d9b7712dc0e678f671
Daniel <dan.laframb@gmail.com>
2016-11-19 14:41:04 -0500

    First commit

---
+++ Cardgame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cardgame
+//
+//  Created by Daniel Laframboise on 2016-11-19.
+//  Copyright © 2016 Daniel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
